f869363ed8d43638b2c2cc7488d8cc218dd6bc9a
Maksim <maksim.vasileuskij@gmail.com>
2024-12-10 00:02:27 +0300

    feat: done one task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -157,8 +157,10 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  const valueEaqualIndex = (element) => element === arr.indexOf(element);
+
+  return arr.some(valueEaqualIndex);
 }
 
 /**
