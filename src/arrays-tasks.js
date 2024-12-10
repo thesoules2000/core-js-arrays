2dc5cf186b3732f5efb8875bca48c14dd8cd529d
Maksim <maksim.vasileuskij@gmail.com>
2024-12-10 22:07:36 +0300

    feat: done findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -490,8 +490,15 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const arrFormed = [];
+  arr1.reduce((current, currentValue) => {
+    if (arr2.includes(currentValue)) {
+      arrFormed.push(currentValue);
+    }
+    return current;
+  });
+  return arrFormed;
 }
 
 /**
