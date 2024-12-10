6459c7d8c3a972c5f049fd92087ca609c9b1ab56
Василевский Максим Евгеньевич <mvasilevsky@mapsoft.by>
2024-12-10 12:23:16 +0300

    feat: done task calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -319,8 +319,10 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (arr.length === 0) return 0;
+  const arrFormed = arr.flatMap((num) => num[0] - num[1]);
+  return arrFormed.reduce((acc, element) => acc + element);
 }
 
 /**
