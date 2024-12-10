45cfebf96c9deb049f25867807a52c0a026194a8
Maksim <maksim.vasileuskij@gmail.com>
2024-12-10 21:54:56 +0300

    feat: done getHexRGBValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -453,8 +453,11 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((x) => {
+    const hex = x.toString(16).toUpperCase().padStart(6, '0');
+    return `#${hex}`;
+  });
 }
 
 /**
