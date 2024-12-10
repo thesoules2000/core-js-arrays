6213c00b6f501c85a2f648c1121fa99661bd715d
Василевский Максим Евгеньевич <mvasilevsky@mapsoft.by>
2024-12-10 10:14:01 +0300

    feat: done with Returns array containing only unique values

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -251,8 +251,9 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const setUnique = new Set(arr);
+  return [...setUnique];
 }
 
 /**
